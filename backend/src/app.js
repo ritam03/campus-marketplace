0a6c1ac3be7c63edeb18574502d2cae12e7abb9b
Ritam Pal <ritam03pal@gmail.com>
2026-03-16 01:46:30 +0530

    Modify CORS dns

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173', 
     'http://localhost:3000',
-    'https://campus-marketplace-three.vercel.app' 
+    'https://campus-marketplace-project.vercel.app' 
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
